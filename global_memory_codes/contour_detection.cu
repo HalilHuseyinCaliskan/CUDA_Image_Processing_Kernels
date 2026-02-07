cc0346d72f7101a67b4dfde30918d32c6cbea769
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2026-02-07 13:44:33 +0300

    Create contour_detection.cu

---
+++ global_memory_codes/contour_detection.cu
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cuda_runtime.h>
+#include "contour_detection_header.cuh"
+
+int main(){
+
+    cv::Mat resim = cv::imread("kedi.png",cv::IMREAD_COLOR);
+
+    if (resim.empty()){
+        return -1;
+    }
+
+    cv::Mat gray, binary;
+    cv::cvtColor(resim,gray,cv::COLOR_BGR2GRAY);
+    cv::threshold(gray,binary,50,255,cv::THRESH_BINARY);
+
+    int width = gray.cols;
+    int height = gray.rows;
+    int size_bgr = width * height * 3 * sizeof(unsigned char);
+    int size = width * height * sizeof(unsigned char);
+    unsigned char *input_matrix, *output_matrix;
+
+    cudaMalloc((void**)&input_matrix,size_bgr);
+    cudaMalloc((void**)&output_matrix,size);
+
+    cudaMemcpy(input_matrix,resim.data,size_bgr,cudaMemcpyHostToDevice);
+    cudaMemcpy(output_matrix,binary.data,size,cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(16,16);
+    dim3 numBlocks((width + threadsPerBlock.x -1)/threadsPerBlock.x,(height + threadsPerBlock.y -1)/threadsPerBlock.y);
+
+    contour_detection<<<numBlocks,threadsPerBlock>>>(input_matrix,output_matrix,width,height);
+
+    cudaDeviceSynchronize();
+
+    cv::Mat contour_detected(height,width,CV_8UC3);
+    cudaMemcpy(contour_detected.data,input_matrix,size_bgr,cudaMemcpyDeviceToHost);
+
+    cv::imshow("frame",contour_detected);
+    cv::imshow("frame2",binary);
+    cv::waitKey(0);
+
+    cudaFree(input_matrix);
+    cudaFree(output_matrix);
+
+    return 0;
+}
